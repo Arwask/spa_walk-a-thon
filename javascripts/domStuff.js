f912e838968a9bdb7c0e6ac4fd1ae40466f1eacb
Arwa <arwa.it15@gmail.com>
2017-07-06 08:54:50 -0500

    getting radio button select

--- javascripts/domStuff.js
+++ javascripts/domStuff.js
@@ -5,7 +5,9 @@ let domStuff = {};
 let fname = document.getElementById('firstName');
 let lname = document.getElementById('lastName');
 let amt = document.getElementById('amount');
-
+let radioDiv = document.getElementById('options')
+let selectedRadio = radioDiv.getElementsByTagName('input');
+console.log(selectedRadio)
 domStuff.getInformation = function()
 {
 	// console.log("in getInformation");
@@ -22,6 +24,18 @@ domStuff.putOnDom = function(donorInfo)
 	var msg = `<tr><td>${donorInfo.firstName} ${donorInfo.lastName}</td><td>${donorInfo.pledge}</td></tr>`;
 	tableDiv.innerHTML += msg;
 }
+
+domStuff.checkOption = function()
+{
+	if(selectedRadio[0].checked)
+			console.log('total selected');
+	else if(selectedRadio[1].checked)
+			console.log('per lap');
+		else
+			console.log('select something')
+
+}
+
 oldDonation.domStuff = domStuff;
 return oldDonation;
 }(donation || {});
